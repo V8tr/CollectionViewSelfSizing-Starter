0e2e65f2f2c3446f6880d1c2db74be6635700dbf
V8tr <ne0_n@mail.ru>
2019-02-01 19:05:04 +0200

    Continue tutorial

--- CollectionViewSelfSizing/CollectionCell.swift
+++ CollectionViewSelfSizing/CollectionCell.swift
@@ -2,7 +2,7 @@
 //  CollectionCell.swift
 //  CollectionViewSelfSizing
 //
-//  Created by Vadym Bulavin on 1/31/19.
+//  Created by Vadym Bulavin on 2/1/19.
 //  Copyright © 2019 Vadym Bulavin. All rights reserved.
 //
 
@@ -10,34 +10,4 @@ import UIKit
 
 class CollectionCell: UICollectionViewCell {
     @IBOutlet weak var titleLabel: UILabel!
-    
-    // Note: must be strong
-    @IBOutlet private var maxWidthConstraint: NSLayoutConstraint! {
-        didSet {
-            maxWidthConstraint.isActive = false
-        }
-    }
-    
-    var maxWidth: CGFloat? = nil {
-        didSet {
-            guard let maxWidth = maxWidth else {
-                return
-            }
-            maxWidthConstraint.isActive = true
-            maxWidthConstraint.constant = maxWidth
-        }
-    }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        contentView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            contentView.leftAnchor.constraint(equalTo: leftAnchor),
-            contentView.rightAnchor.constraint(equalTo: rightAnchor),
-            contentView.topAnchor.constraint(equalTo: topAnchor),
-            contentView.bottomAnchor.constraint(equalTo: bottomAnchor)
-        ])
-    }
 }
--- CollectionViewSelfSizing/ViewController.swift
+++ CollectionViewSelfSizing/ViewController.swift
@@ -25,31 +25,3 @@ class ViewController: UIViewController {
         Item(title: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
     ]
 }
-
-// MARK: - Collection view delegate and data source methods
-
-extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return items.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.reuseID, for: indexPath) as! CollectionCell
-        
-        cell.titleLabel.text = items[indexPath.item].title
-        cell.layer.borderWidth = Constants.borderWidth
-        cell.layer.borderColor = UIColor.lightGray.cgColor
-        cell.maxWidth = collectionView.bounds.width - Constants.spacing
-        
-        return cell
-    }
-}
-
-// MARK: - Constants
-
-private enum Constants {
-    static let spacing: CGFloat = 16
-    static let borderWidth: CGFloat = 0.5
-    static let reuseID = "CollectionCell"
-}
